3acc39b2e69edd73abfa12279f58ee8a8a7bbb15
marcelovelar <fernandomarcel1996@gmail.com>
2025-12-20 20:13:56 -0300

    commit

--- src/main/java/com/ecommerce/application/service/OrderService.java
+++ src/main/java/com/ecommerce/application/service/OrderService.java
@@ -2,7 +2,6 @@ package com.ecommerce.application.service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
